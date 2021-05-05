d035f9d927aa127ec16af6ec1cc4a20d3b17e435
Lázaro Alves <geo.eolo@gmail.com>
2021-05-04 22:34:27 -0300

    Encurtador de URL

---
+++ url-shortener-dio/src/config/Constants.ts
@@ -0,0 +1,6 @@
+export const config = {
+  API_URL: "http://localhost:5000",
+  MONGO_CONNECTION:
+    'mongodb+srv://lazaro:R48h5wFwgnpLQyk@cluster0.uwxbo.mongodb.net/url-shortener-dio?retryWrites=true&w=majority'
+
+}
